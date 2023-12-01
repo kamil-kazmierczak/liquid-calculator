837fbec55fd3e0130ccbe85462bed56a23af88be
kmilo <kamil.kazmierczak@pm.me>
2023-12-01 18:15:19 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    Text,
+    Button,
+    Box,
+    Divider,
+    Input,
+    InputField,
+    ButtonText,
+    FormControlLabelText, FormControlLabel, FormControl,
+} from "@gluestack-ui/themed";
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const glycerinDensity = 1.26
+    const glycolDensity = 1.04
+
+    const millilitersOfGlycerinPerNicotineMilligram = 0.039
+    const millilitersOfGlycolPerNicotineMilligram = 0.017
+
+    const initialCalculations = {
+        amountOfLiquid: 0.0,
+        powerOfNicotine: 0.0,
+        amountOfAroma: 0.0,
+        wantedPower: 0.0,
+        glycerinFromNicotine: 0.0,
+        glycolFromNicotine: 0.0,
+        nicotine: 0.0,
+        glycerin: 0.0,
+        glycol: 0.0,
+    }
+
+    const [calc, setCalc] = useState(initialCalculations);
+
+
+    const calculate = () => {
+        setCalc(prevState => ({
+            ...prevState,
+            glycerinFromNicotine: millilitersOfGlycerinPerNicotineMilligram * (prevState.amountOfLiquid * prevState.wantedPower),
+            glycolFromNicotine: millilitersOfGlycolPerNicotineMilligram * (prevState.amountOfLiquid * prevState.wantedPower),
+            nicotine: prevState.amountOfLiquid * prevState.wantedPower / prevState.powerOfNicotine,
+        }))
+        setCalc(prevState => ({
+            ...prevState,
+            glycerin: 0.7 * prevState.amountOfLiquid - prevState.glycerinFromNicotine - (prevState.amountOfAroma * 0.7),
+            glycol: 0.3 * prevState.amountOfLiquid - prevState.glycolFromNicotine - (prevState.amountOfAroma * 0.3),
+        }))
+
+
+    };
+
+    return (
+        <Box paddingHorizontal="$10" flex={1} pt="$20" >
+            <Box flex={3}>
+                <FormControl
+                    size="md"
+                    isDisabled={false}
+                    isInvalid={false}
+                    isReadOnly={false}
+                    isRequired={false}
+                >
+                    <FormControlLabel mb="$2">
+                        <FormControlLabelText>Total amount of liquid [ml]</FormControlLabelText>
+                    </FormControlLabel>
+                    <Input mb="$5"
+                           variant="rounded">
+                        <InputField
+                            keyboardType="numeric"
+                            onChangeText={(value) => setCalc(prev => ({...prev, amountOfLiquid: Number(value)}))}
+                            placeholder="Total amount of liquid [ml]"
+                        />
+                    </Input>
+                    <FormControlLabel mb="$2">
+                        <FormControlLabelText>Power of nicotine [mg/ml]</FormControlLabelText>
+                    </FormControlLabel>
+                    <Input mb="$5"
+                           variant="underlined">
+                        <InputField
+                            keyboardType="numeric"
+                            onChangeText={(value) => setCalc(prev => ({...prev, powerOfNicotine: Number(value)}))}
+                            placeholder="Power of nicotine [mg/ml]"
+                        />
+                    </Input>
+                    <FormControlLabel mb="$2">
+                        <FormControlLabelText>Amount of aroma [ml]</FormControlLabelText>
+                    </FormControlLabel>
+                    <Input mb="$5">
+                        <InputField
+                            keyboardType="numeric"
+                            onChangeText={(value) => setCalc(prev => ({...prev, amountOfAroma: Number(value)}))}
+                            placeholder="Amount of aroma [ml]"
+                        />
+                    </Input>
+                    <FormControlLabel mb="$2">
+                        <FormControlLabelText>Wanted power [mg/ml]</FormControlLabelText>
+                    </FormControlLabel>
+                    <Input>
+                        <InputField
+                            keyboardType="numbers-and-punctuation"
+                            onChangeText={(value) => setCalc(prev => ({...prev, wantedPower: Number(value)}))}
+                            placeholder="Wanted power [mg/ml]"
+                        />
+                    </Input>
+                </FormControl>
+
+            </Box>
+            <Box flex={1}>
+                <Divider padding={1} marginVertical={20}/>
+
+                <Text>Glycerin from nicotine: {(calc.glycerinFromNicotine).toFixed(2)} ml</Text>
+                <Text>Glycol from nicotine: {(calc.glycolFromNicotine).toFixed(2)} ml</Text>
+                <Text>Glycerin from aroma: {(0.7 * calc.amountOfAroma).toFixed(2)} ml</Text>
+                <Text>Glycol from aroma: {(0.3 * calc.amountOfAroma).toFixed(2)} ml</Text>
+            </Box>
+
+            <Box flex={2}>
+                <Divider padding={1} marginVertical={20}/>
+
+                <Text>Nicotine: {calc.nicotine.toFixed(2)} ml ({(calc.glycerinFromNicotine * glycerinDensity + calc.glycolFromNicotine * glycolDensity).toFixed(2)} g)</Text>
+                <Text>Glycerin: {calc.glycerin.toFixed(2)} ml ({(calc.glycerin * glycerinDensity).toFixed(2)} g)</Text>
+                <Text>Glycol: {calc.glycol.toFixed(2)} ml ({(calc.glycol * glycolDensity).toFixed(2)} g)</Text>
+
+                <Button mt="$16" onPress={calculate}>
+                    <ButtonText>Calculate</ButtonText>
+                </Button>
+            </Box>
+        </Box>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import {GluestackUIProvider} from "@gluestack-ui/themed";
+import {config} from "@gluestack-ui/config";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <GluestackUIProvider config={config}>
+            <App/>
+        </GluestackUIProvider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
